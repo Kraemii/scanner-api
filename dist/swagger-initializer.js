d272d9c59d1764ad243aa87fd713d3d73d25dff4
Christoph Krämer <c.kraemer.rulle@web.de>
2023-09-19 13:52:45 +0200

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,11 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/greenbone/openvas-scanner/main/rust/doc/openapi.yml",
+    urls: [
+      {url: "https://raw.githubusercontent.com/greenbone/openvas-scanner/main/rust/doc/openapi.yml", name: "Scanner API"},
+      {url: "https://raw.githubusercontent.com/greenbone/openvas-scanner/fd617e86b2cfc1e083905b5672081c51cd9150d9/rust/doc/reverse-sensor-openapi.yml", name: "Reverse Scanner API"}
+    ],
+    "urls.primaryName": "Scanner API",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
